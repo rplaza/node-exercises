b279a09ca1e75014d30ea9d9d473a8ec298ff9ec
rplaza <raul.plaza@gmail.com>
2015-02-10 13:09:18 +0000

    Filtered ls solved

--- filteredLs/filteredLs.js
+++ filteredLs/filteredLs.js
@@ -1,13 +1,16 @@
 
 var fs = require('fs');
+var path = require('path');
 var extension = '.' + process.argv[3];
 
-fs.readFile(process.argv[2],callback);
+fs.readdir(process.argv[2],callback);
 
-function callback(err, data) {
+function callback(err, list) {
 	if(!err) {
-		data.filter(function(file) {
-			if(file)
+		list.filter(function(file) {
+			if(path.extname(file) == extension) {
+				console.log(file);	
+			}
 		})
 	}	
 } 
